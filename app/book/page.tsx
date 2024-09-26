412d4cf4226b45d0726b4fc0d692e05f8672aadc
Viktorlyt <viktorlyt@gmail.com>
2024-09-26 13:27:30 -0600

    calendly event trigger added

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,10 +1,58 @@
 "use client";
 
-import { InlineWidget } from "react-calendly";
+import { EventScheduledEvent, InlineWidget } from "react-calendly";
 
 import Navbar from "@/components/navbar";
+import { useEffect } from "react";
 
 const Book = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    script.onload = () => {
+      try {
+        // Initialize the Calendly widget
+        if (window.Calendly) {
+          window.Calendly.initInlineWidget({
+            url: "https://calendly.com/p-demenshyn-theclicks",
+            parentElement: document.getElementById("calendly-container"),
+            prefill: {},
+            utm: {},
+          });
+
+          // Set up the event listener for Calendly events
+          window.Calendly.setupWidget({
+            url: "https://calendly.com/p-demenshyn-theclicks",
+            parentElement: document.getElementById("calendly-container"),
+            prefill: {},
+            utm: {},
+            onEvent: function (e: EventScheduledEvent) {
+              if (e.data.event === "calendly.event_scheduled") {
+                window.dataLayer?.push({
+                  event: "CalendlyEventScheduled",
+                  calendly_event_uri: e.data.payload.event.uri,
+                  calendly_invitee_uri: e.data.payload.invitee.uri,
+                });
+              }
+            },
+          });
+        } else {
+          throw new Error("Calendly script not loaded properly.");
+        }
+      } catch (error) {
+        console.error("Error initializing Calendly widget:", error);
+      }
+    };
+
+    document.body.appendChild(script);
+
+    // Clean up the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <div className="flex flex-col   w-full  h-screen bg-[var(--bg-96)]  bg-grid-white/[0.02]   ">
@@ -16,7 +64,9 @@ const Book = () => {
           <h1>Book a meeting</h1>
         </div>
 
-        <InlineWidget url="https://calendly.com/p-demenshyn-theclicks/consultation" />
+        <div id="calendly-container">
+          <InlineWidget url="https://calendly.com/p-demenshyn-theclicks/consultation" />
+        </div>
       </div>
     </>
   );
