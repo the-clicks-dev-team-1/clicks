4336bb754a9837e9f61d9fd4cb9a394c3fb07cc1
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2025-01-17 20:40:50 -0400

    offers translated

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -4,200 +4,88 @@ import { useState } from "react";
 import { PiCheckCircleFill } from "react-icons/pi";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import en from "@/i18n/messages/en.json";
 import { Link } from "@/i18n/routing";
 
 export interface Offer {
-  index: number;
+  style: string;
+  image: string;
   name: string;
   price: string;
-  price_description: string;
+  priceDescription: string;
   features: string[];
-  style: string;
   description: string;
   button: string;
-  image: string;
 }
 
-const offers = [
+const offersBase = [
   {
-    index: 0,
-    name: "Initial Consultation & Strategy Development",
-    price: "CAD $1,000 - $3,000",
-    price_description: "One-time fee",
-    features: [
-      "Business analysis",
-      "Market research",
-      "Competitor analysis",
-      "Strategy document",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Detailed consultation to understand the client's business goals, target audience, and current marketing efforts. Development of a customized digital marketing strategy.",
-    button: "Order Now",
     image: "/images/Group1.png",
   },
   {
-    index: 1,
-    name: "Website Design & Development",
-    price: "CAD $5,000 - $20,000",
-    price_description: "One-time fee \n (depending on complexity)",
-    features: [
-      "Custom design",
-      "Development (frontend and backend)",
-      "Content management system (CMS) setup",
-      "Basic SEO optimization",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Creation of a modern, responsive website optimized for user experience and conversions.",
-    button: "Order Now",
     image: "/images/Group2.png",
   },
   {
-    index: 2,
-    name: "Search Engine Optimization (SEO)",
-    price: "CAD $1,000 - $5,000",
-    price_description: "Monthly retainer",
-    features: [
-      "Keyword research",
-      "On-page optimization",
-      "Link building",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "On-page and off-page SEO to improve search engine rankings and organic traffic.",
-    button: "Order Now",
     image: "/images/Group3.png",
   },
   {
-    index: 3,
-    name: "Pay-Per-Click (PPC) Advertising",
-    price: "CAD $1,500 - $10,000",
-    price_description: "Monthly retainer \n (excluding ad spend)",
-    features: [
-      "Google Ads setup and management",
-      "Bing Ads setup and management",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Management of PPC campaigns to drive targeted traffic and increase conversions.",
-    button: "Order Now",
     image: "/images/Group1.png",
   },
   {
-    index: 4,
-    name: "Social Media Marketing",
-    price: "CAD $1,000 - $7,000",
-    price_description: "Monthly retainer",
-    features: [
-      "Content calendar",
-      "Post creation and scheduling",
-      "Community engagement",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Social media strategy, content creation, and community management across platforms like Facebook, Instagram, Twitter, LinkedIn, and others.",
-    button: "Order Now",
     image: "/images/Group2.png",
   },
   {
-    index: 5,
-    name: "Content Marketing",
-    price: "CAD $2,000 - $8,000",
-    price_description: "Monthly retainer",
-    features: [
-      "Blog posts",
-      "Articles",
-      "Infographics",
-      "E-books/Whitepapers",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Creation and distribution of valuable content to attract and engage the target audience.",
-    button: "Order Now",
     image: "/images/Group3.png",
   },
   {
-    index: 6,
-    name: "Email Marketing",
-    price: "CAD $1,000 - $5,000",
-    price_description: "Monthly retainer",
-    features: [
-      "Email template design",
-      "List segmentation",
-      "Campaign management",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Design and execution of email marketing campaigns to nurture leads and retain customers.",
-    button: "Order Now",
     image: "/images/Group1.png",
   },
   {
-    index: 7,
-    name: "Conversion Rate Optimization (CRO)",
-    price: "CAD $1,500 - $7,000",
-    price_description: "Monthly retainer",
-    features: [
-      "A/B testing",
-      "User behavior analysis",
-      "Conversion funnel analysis",
-      "Monthly performance reports",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Analysis and optimization of website and landing pages to improve conversion rates.",
-    button: "Order Now",
     image: "/images/Group2.png",
   },
   {
-    index: 8,
-    name: "Analytics & Reporting",
-    price: "CAD $1,000 - $3,000",
-    price_description: "Monthly retainer",
-    features: [
-      "Google Analytics setup",
-      "Monthly performance reports",
-      "Quarterly strategy reviews",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent* text-[var(--text-label)] bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Comprehensive analytics and reporting to track the performance of digital marketing efforts.",
-    button: "Order Now",
     image: "/images/Group3.png",
   },
   {
-    index: 9,
-    name: "Marketing Automation",
-    price: "CAD 2,000 - $10,000 \n + $1,000 - $5,000",
-    price_description: "One-time setup fee \n + Monthly retainer",
-    features: [
-      "Tool setup and integration",
-      "Workflow creation",
-      "Training and support",
-    ],
     style:
       "rounded-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-300 to-neutral-50",
-    description:
-      "Implementation of marketing automation tools to streamline and enhance marketing efforts.",
-    button: "Order Now",
     image: "/images/Group1.png",
   },
 ];
+
+const offers: Offer[] = offersBase.map((base, index) => {
+  const featuresLength = en.pricing.offers.offersList[index].features.length;
+  return {
+    ...base,
+    name: `offersList.${index}.name`,
+    price: `offersList.${index}.price`,
+    priceDescription: `offersList.${index}.priceDescription`,
+    features: Array.from(
+      { length: featuresLength },
+      (_, i) => `offersList.${index}.features.${i}`
+    ),
+    description: `offersList.${index}.description`,
+    button: `offersList.${index}.button`,
+  };
+});
 
 export default function Offers() {
   const t = useTranslations("pricing.offers");
@@ -210,7 +98,7 @@ export default function Offers() {
   return (
     <>
       <div className="grid gap-6 px-4 md:px-10 py-0 max-w-7xl cursor-pointer items-center">
-        {offers.slice(0, visibleOffers).map((offer) => (
+        {offers.slice(0, visibleOffers).map((offer, index) => (
           <div key={offer.name} className="flex flex-col">
             <div className="flex flex-col md:flex-row">
               <div className="px-20 py-20 light:bg-[var(--light-blue)] light:shadow-lg bg-[var(--blue2)] rounded-3xl w-full max-w-screen-xl relative">
@@ -219,19 +107,19 @@ export default function Offers() {
                     <div
                       className={`text-3xl md:text-4xl font-medium ${offer.style} text-[var(--light-blue)] light:text-[var(--gray-70)] py-0`}
                     >
-                      {offer.name}
+                      {t(`${offer.name}`)}
                     </div>
                     <div className="py-4 md:py-6 text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-                      {offer.description}
+                      {t(`${offer.description}`)}
                     </div>
                     <ul>
-                      {offer.features.map((feature) => (
+                      {offer.features.map((feature, i) => (
                         <li
                           key={feature}
                           className="text-lg py-2 flex space-x-2 items-center text-[var(--gray-blue)] light:text-[var(--gray-40)]"
                         >
                           <PiCheckCircleFill className="text-green-600 mr-2 text-xl" />
-                          {feature}
+                          {t(feature)}
                         </li>
                       ))}
                     </ul>
@@ -250,17 +138,17 @@ export default function Offers() {
 
               <div className="md:w-1/3 px-8 animate-shimmer items-center justify-center light:shadow-lg rounded-3xl dark:border border-slate-800 light:border-[var(--ocean-blue)] dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] light:bg-[linear-gradient(110deg,#f8fbff,45%,#edf8fe,55%,#f8fbff)] light:bg-[var(--light-blue)] bg-[length:200%_100%] transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <div className="whitespace-pre-line text-xl pt-4 md:pt-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
-                  {offer.price}
+                  {t(`${offer.price}`)}
                 </div>
                 <div className="whitespace-pre-line text-sm pt-2 text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-                  {offer.price_description}
+                  {t(`${offer.priceDescription}`)}
                 </div>
                 <div className="mt-4">
                   <Link
                     href="/contact"
                     className="rounded-lg bg-[var(--ocean-blue)] my-4 py-2 text-white w-full mx-auto items-center flex justify-center"
                   >
-                    Order Now
+                    {t("orderNow")}
                   </Link>
                 </div>
               </div>
@@ -273,7 +161,7 @@ export default function Offers() {
           onClick={handleLoadMore}
           className="mt-10 px-4 py-2 bg-[var(--ocean-blue)] text-white rounded-lg"
         >
-          Load More
+          {t("loadMore")}
         </button>
       )}
     </>
