624545edeb03a5b1f16554bae34967e2588833ce
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2025-01-19 17:43:18 -0400

    services/seo page translated

--- app/[locale]/services/seo/page.tsx
+++ app/[locale]/services/seo/page.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-import Image from "next/image";
 import { Metadata } from "next";
+import Image from "next/image";
+import { getTranslations } from "next-intl/server";
 import ContactBlock from "@/components/contactBlock";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 import ServiceCard from "@/components/ServiceCard";
+import en from "@/i18n/messages/en.json";
 
 export const metadata: Metadata = {
   title: "Search Engine Optimization (SEO) Services",
@@ -12,7 +14,7 @@ export const metadata: Metadata = {
     "Increase your online visibility and drive more traffic to your website with our expert SEO services",
 };
 
-const servicesItems = [
+const servicesItems2 = [
   {
     count: "01",
     image: "/images/Group1.png",
@@ -57,7 +59,18 @@ const servicesItems = [
   },
 ];
 
-const SEO: FC = () => {
+const servicesItems = en.services.seo.items.map((_, index) => ({
+  count: `0${index + 1}`,
+  image: `/images/Group${index + 1}.png`,
+  title: `items.${index}.title`,
+  description: `items.${index}.description`,
+}));
+
+const SEO: FC<{ params: { locale: string } }> = async ({
+  params: { locale },
+}) => {
+  const t = await getTranslations({ locale, namespace: "services.seo" });
+
   return (
     <div className="bg-[var(--bgnew)] text-[var(--text)]">
       <Navbar />
@@ -65,14 +78,10 @@ const SEO: FC = () => {
       <div className="container md:px-10 mb-32">
         <div className="flex flex-col pt-32 md:pt-40">
           <h1 className="mb-10 py-2 text-3xl md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to bg-purple-500 bg-opacity-50">
-            Search Engine Optimization (SEO) Services
+            {t("mainSection.title")}
           </h1>
           <p className="mt-3 text-lg md:text-xl max-w-2xl text-left text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-            Increase your online visibility and drive more traffic to your
-            website with our expert SEO services. Our SEO services focus on
-            improving your website&apos;s visibility on search engines like
-            Google, helping potential customers find you when they&apos;re
-            searching for your products or services.
+            {t("mainSection.description")}
           </p>
         </div>
       </div>
@@ -85,8 +94,8 @@ const SEO: FC = () => {
               key={item.count}
               count={item.count}
               image={item.image}
-              title={item.title}
-              description={item.description}
+              title={t(item.title)}
+              description={t(item.description)}
             />
           ))}
         </div>
@@ -97,38 +106,33 @@ const SEO: FC = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           <div>
             <h2 className="text-2xl md:text-3xl font-semibold mb-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
-              What is SEO and Why Does It Matter?
+              {t("what.title")}
             </h2>
             <p className="text-lg mb-6 text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-              SEO, or Search Engine Optimization, is the practice of improving
-              your website&apos;s visibility on search engines like Google. When
-              people search for products or services that you offer, you want
-              your website to appear at the top of the results. SEO helps make
-              that happen.
+              {t("what.description")}
             </p>
             <h2 className="text-lg md:text-xl font-semibold mb-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
-              By optimizing your website for search engines, you can increase:
+              {t("why.title")}
             </h2>
             <div className="flex flex-col sm:flex-row md:flex-col gap-2 xl:flex-row justify-between items-center mb-6 text-lg text-[var(--gray-blue)] light:text-[var(--gray-40)]">
               <div className="w-full inline-block bg-gradient-to-r from-sky-400 to-purple-500 p-[1px] rounded-full">
                 <span className="min-w-[148px] whitespace-nowrap w-full block bg-white dark:bg-black rounded-full px-4 py-3 text-center text-lg font-semibold">
-                  visibility
+                  {t("why.benefitsList.0")}
                 </span>
               </div>
               <div className="w-full inline-block bg-gradient-to-r from-sky-400 to-purple-500 p-[1px] rounded-full">
                 <span className="min-w-[148px] whitespace-nowrap w-full text-center block bg-white dark:bg-black  rounded-full px-4 py-3 text-lg font-semibold">
-                  traffic
+                  {t("why.benefitsList.1")}
                 </span>
               </div>
               <div className="w-full inline-block bg-gradient-to-r from-sky-400 to-purple-500 p-[1px] rounded-full">
                 <span className="min-w-[148px] whitespace-nowrap w-full text-center block bg-white dark:bg-black  rounded-full px-4 py-3 text-lg font-semibold">
-                  user experience
+                  {t("why.benefitsList.2")}
                 </span>
               </div>
             </div>
             {/* <p className="text-lg text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-              A well-defined strategy is essential for the growth and
-              sustainability of any business.
+                           {t("why.description")}
             </p> */}
           </div>
           <div className="relative w-full h-64 md:h-96">
@@ -143,7 +147,7 @@ const SEO: FC = () => {
         </div>
       </div>
 
-      <ContactBlock consultation="Reach out for a free consultation and get the answers you need." />
+      <ContactBlock consultation={t("contactFormMessage")} />
 
       <Footer />
     </div>
