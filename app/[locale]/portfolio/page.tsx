8aa0313f6b5a8b2cf0533e83f6908b013bbf7fcc
Viktorlyt <viktorlyt@gmail.com>
2025-01-26 18:10:46 -0700

    portfolio page added

---
+++ app/[locale]/portfolio/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+const Portfolio = () => {
+  return (
+    <div className="bg-[var(--bgnew)] light:bg-white text-[var(--light-blue)] light:text-[var(--gray-70)]">
+      <Navbar />
+      <div className="h-screen flex justify-center items-center">
+        <p className="self-center">Portfolio under construction...</p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Portfolio;
