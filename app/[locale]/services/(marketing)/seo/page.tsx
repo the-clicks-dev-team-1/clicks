ce08053544386270851d582f900fc5f2ee0a47d3
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2025-01-30 09:22:51 -0400

    misc changes

--- app/[locale]/services/(marketing)/seo/page.tsx
+++ app/[locale]/services/(marketing)/seo/page.tsx
@@ -48,17 +48,27 @@ const SEO: FC<{ params: { locale: string } }> = async ({
       <div className="container md:px-10 mb-32 pt-32 md:pt-40">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           <div>
+            <div className="mb-10">
+              <div className="flex flex-col">
+                <h1 className="mb-1 py-2 text-3xl md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to bg-purple-500 bg-opacity-50">
+                  {t("mainSection.title")}
+                </h1>
+                <p className="mt-3 text-lg md:text-xl max-w-2xl text-left text-[var(--gray-blue)] light:text-[var(--gray-40)]">
+                  {t("mainSection.description")}
+                </p>
+              </div>
+            </div>
             <h2 className="text-2xl md:text-3xl font-semibold mb-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
               {t("what.title")}
             </h2>
             <p className="text-lg mb-6 text-[var(--gray-blue)] light:text-[var(--gray-40)]">
               {t("what.description")}
             </p>
-            <h2 className="text-lg md:text-xl font-semibold mb-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
+            {/* <h2 className="text-lg md:text-xl font-semibold mb-6 text-[var(--light-blue)] light:text-[var(--gray-70)]">
               {t("why.title")}
-            </h2>
+            </h2> */}
 
-            <div className="flex flex-col sm:flex-row md:flex-col gap-2 xl:flex-row justify-between mb-6 text-lg text-[var(--gray-blue)] light:text-[var(--gray-40)]">
+            {/* <div className="flex flex-col sm:flex-row md:flex-col gap-2 xl:flex-row justify-between mb-6 text-lg text-[var(--gray-blue)] light:text-[var(--gray-40)]">
               <div className="flex-1 flex">
                 <div className="h-full w-full inline-block bg-gradient-to-r from-sky-400 to-purple-500 p-[1px] rounded-full">
                   <span className="h-full min-w-[148px] w-full flex items-center justify-center text-center block bg-white dark:bg-black rounded-full px-4 py-3 text-lg font-semibold">
@@ -82,10 +92,10 @@ const SEO: FC<{ params: { locale: string } }> = async ({
                   </span>
                 </div>
               </div>
-            </div>
+            </div> */}
 
             {/* <p className="text-lg text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-                           {t("why.description")}
+              {t("why.description")}
             </p> */}
           </div>
           <div className="relative w-full h-64 md:h-96">
@@ -112,17 +122,6 @@ const SEO: FC<{ params: { locale: string } }> = async ({
               description={t(item.description)}
             />
           ))}
-        </div>
-      </div>
-
-      <div className="container md:px-10 mb-32">
-        <div className="flex flex-col">
-          <h1 className="mb-10 py-2 text-3xl md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to bg-purple-500 bg-opacity-50">
-            {t("mainSection.title")}
-          </h1>
-          <p className="mt-3 text-lg md:text-xl max-w-2xl text-left text-[var(--gray-blue)] light:text-[var(--gray-40)]">
-            {t("mainSection.description")}
-          </p>
         </div>
       </div>
 
