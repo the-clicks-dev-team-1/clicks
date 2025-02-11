f5ffef0ab21716cd9e6c0e5c939c7f902bbe96a7
Borys Zahorodnii <boriszagorodniy@gmail.com>
2025-02-11 13:30:12 -0400

    feat: ga setup

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { getMessages } from "next-intl/server";
 import { ThemeProvider } from "next-themes";
 import { Poppins } from "next/font/google";
 import "../globals.css";
+import Script from "next/script";
 import { GoogleTagManager } from "@next/third-parties/google";
 import FacebookPixel from "@/components/FacebookPixel";
 import LiveChat from "@/components/LiveChat";
@@ -43,19 +44,33 @@ export default async function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
-        <GoogleTagManager gtmId="GTM-588PZWKW" />
+        {/* <GoogleTagManager gtmId="GTM-588PZWKW" /> */}
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={font.className} suppressHydrationWarning>
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-588PZWKW"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
+        </noscript> */}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4HWD3L1B9T"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4HWD3L1B9T');
+          `}
+        </Script>
+        
         <FacebookPixel />
         <ThemeProvider
           attribute="class"
@@ -70,6 +85,7 @@ export default async function RootLayout({
         </ThemeProvider>
         <LiveChat />
         <ScrollToTop />
+
       </body>
     </html>
   );
